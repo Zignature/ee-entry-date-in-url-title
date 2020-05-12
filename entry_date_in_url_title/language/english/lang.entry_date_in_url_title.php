fb5b004a6fefd134165a7c24ff3e38a3de96d03c
Zignature <siegert.naber@gmail.com>
2020-05-12 23:54:37 +0200

    Create lang.entry_date_in_url_title.php

---
+++ entry_date_in_url_title/language/english/lang.entry_date_in_url_title.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang = array(
+
+//----------------------------------------
+// Required for settings
+//----------------------------------------
+
+    'chosen_channels' => 'Choose which channels should have the dates added to the url titles.',
+    'position_of_date' => 'Choose the location of the entry\'s Date.',
+    'prepend' => 'Prepend to url_title (ex: "2020-05-13--my-entry-title")',
+    'append' => 'Append to url_title (ex: "my-entry-title--2020-05-13")',
+
+// END
+    ''=>''
+);
