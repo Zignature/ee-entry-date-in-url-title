f2a1ccad180168055beeae5bc4b5e10059667078
Zignature <siegert.naber@gmail.com>
2020-05-12 22:47:44 +0200

    Create addon.setup.php

---
+++ entry_date_in_url_title/addon.setup.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * 
+ * This is a fork of the "Entry ID in URL_Title" plugin by mattnap.
+ * https://github.com/mattnap/entry-id-in-url-title-ee
+ * 
+ * Instead of prepending/appending the Entry ID, this plugin prepends/appends
+ * the Entry Date into the URL Title.
+ * 
+ * This version is compatible with EE3+.
+ * 
+ */
+
+return array(
+    'author'      => 'Zignature',
+    'author_url'  => 'https://github.com/Zignature/',
+    'name'        => 'Entry Date in URL Title',
+    'description' => 'On publish prepends or appends the entry date to the url title.',
+    'version'     => '1.0.0',
+    'namespace'   => 'zignature\EntryDateInUrlTitle',
+    'settings_exist' => TRUE
+);
